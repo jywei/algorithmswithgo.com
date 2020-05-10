a0b6bbc2f97f1e4e686269b54503864b51040edc
Roy Wei <roywjy@gmail.com>
2020-05-10 16:12:25 +1000

    Add Reverse solutions

--- module01/reverse.go
+++ module01/reverse.go
@@ -6,6 +6,37 @@ package module01
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+// func Reverse(word string) string {
+// 	var res string
+// 	for i := len(word) - 1; i >= 0; i-- {
+// 		res += string(word[i])
+// 	}
+// 	return res
+// }
+
+// Reverse will return the provided word in reverse
+// func Reverse(word string) string {
+// 	var sb strings.Builder
+// 	for i := len(word) - 1; i >= 0; i-- {
+// 		sb.WriteByte(word[i])
+// 	}
+// 	return sb.String()
+// }
+
+// Reverse will return the provided word in reverse
+// func Reverse(word string) string {
+// 	var res string
+// 	for i := 0; i < len(word); i++ {
+// 		res = string(word[i]) + res
+// 	}
+// 	return res
+// }
+
+// Reverse will return the provided word in reverse with rune
 func Reverse(word string) string {
-	return ""
+	var res string
+	for _, r := range word {
+		res = string(r) + res
+	}
+	return res
 }
