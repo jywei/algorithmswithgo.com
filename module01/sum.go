5b2a98c39f1d414fafdd31581e0016df9e04b02d
Roy Wei <roywjy@gmail.com>
2020-05-10 15:57:21 +1000

    Add recursive approach for sum

--- module01/sum.go
+++ module01/sum.go
@@ -2,10 +2,19 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _, val := range numbers {
+// 		sum += val
+// 	}
+// 	return sum
+// }
+
+// Sum will sum up all of the numbers passed
+// with recursion approach
 func Sum(numbers []int) int {
-	sum := 0
-	for _, val := range numbers {
-		sum += val
+	if len(numbers) == 0 {
+		return 0
 	}
-	return sum
+	return numbers[0] + Sum(numbers[1:])
 }
